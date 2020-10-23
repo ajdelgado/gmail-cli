b91b642071e455d20513f970c8907a5f485c6789
Antonio J. Delgado <antoniodelgado@susurrando.com>
2020-10-23 10:58:31 +0300

    added initial script

---
+++ README.md
@@ -0,0 +1,10 @@
+# GmailCLI
+A command line Google Mail client
+
+## Usage
+gmailCLI.py [--query|-q 'is:unread']
+
+- query: A Google Mail query to run, instead of listing all emails
+
+## Google documentation
+https://developers.google.com/resources/api-libraries/documentation/gmail/v1/python/latest/gmail_v1
---
+++ gmail-cli.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# This script is licensed under GNU GPL version 3.0
+# (c) 2020 Antonio J. Delgado
+import json
+import pickle
+import os
+import click
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+@click.command()
+@click.option('--query', '-q', help='Google Mail query to run')
+def get_messages(query):
+    home_folder = os.environ.get('HOME', os.environ.get('USERPROFILE',''))
+    with open(os.path.join(home_folder, '.local', 'client_secret_gmail.json'), 'r') as cred_file:
+        credentials = json.load(cred_file)
+    SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_config(
+                credentials,
+                scopes=SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    service = build('gmail', 'v1', credentials=creds)
+
+    #results = service.users().labels().list(userId='me').execute()
+    results = service.users().messages().list(userId='me', q=query).execute()
+    for iter_message in results.get('messages', list()):
+        gmessage = service.users().messages().get(userId='me', format='full', id=iter_message['id']).execute()
+        message = dict()
+        payload = gmessage.get('payload',dict())
+        headers = payload.get('headers', list())
+        for header in headers:
+            if 'name' in header and 'value' in header:
+                message[header['name']] = header['value']
+        for key in gmessage.keys():
+            message[key] = gmessage[key]
+        #print(f"({iter_message['id']}) {subject}")
+        print(json.dumps(message, indent=2))
+
+if __name__ == '__main__':
+    get_messages()
